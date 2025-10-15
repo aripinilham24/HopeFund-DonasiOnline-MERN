95637018f882a97d96d42d1c479d063d3be4b30a
Aripin Ilham <muhamadarifinilham24@gmail.com>
2025-10-15 09:18:56 +0700

    chore: update donate routes and controller logic

--- server/controllers/donateController.js
+++ server/controllers/donateController.js
@@ -1,6 +1,16 @@
 import snap from "../config/midtrans.js";
 import Transaction from "../models/Transaction.js";
 
+export const getConfig = async (req, res) => {
+    try {
+        const clientKey = process.env.MIDTRANS_CLIENT_KEY;
+        res.json({ clientKey });
+    } catch (err) {
+        console.error("❌ Error fetching client key:", err.message);
+        res.status(500).json({ message: "Gagal mengambil client key" });
+    }
+};
+
 export const createSnapTransaction = async (req, res) => {
     try {
         const { donateId, amount, message, customer, campaignId } = req.body;
@@ -89,4 +99,35 @@ export const handleMidtransNotification = async (req, res) => {
         return res.status(500).json({ message: "Error" });
     }
 };
- 
+
+export const notification = async (req, res) => {
+    try {
+        const notification = req.body;
+
+        const { order_id, transaction_status, fraud_status } = notification;
+
+        // Cari transaksi berdasarkan order_id
+        const tx = await Transaction.findOne({ donateId: order_id });
+        if (!tx) {
+            return res
+                .status(404)
+                .json({ message: "Transaksi tidak ditemukan" });
+        }
+
+        if (transaction_status === "settlement" && fraud_status === "accept") {
+            tx.status = "success";
+        } else if (transaction_status === "pending") {
+            tx.status = "pending";
+        } else {
+            tx.status = "failed";
+        }
+
+        await tx.save();
+
+        // harus balas 200 ke Midtrans
+        return res.status(200).json({ message: "OK" });
+    } catch (err) {
+        console.error("Error notification:", err);
+        return res.status(500).json({ message: "Error" });
+    }
+};
