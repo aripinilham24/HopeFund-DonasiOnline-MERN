103f78657dff7225aa19b3f0c50a9cd117c35866
Aripin Ilham <muhamadarifinilham24@gmail.com>
2025-10-17 23:29:48 +0700

    wip(authentication): update userschema for oauth

--- server/models/User.js
+++ server/models/User.js
@@ -6,7 +6,9 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true, lowercase: true, trim: true },
   passwordHash: { type: String, required: true },
   role: { type: String, enum: ["admin", "user"], default: "user" },
-  avatar: { type: String }
+  avatar: { type: String },
+  googleId: { type: String, default: null },
+  provider: { type: String, enum: ['local', 'google'], default: 'local' },
 }, { timestamps: true });
 
 
