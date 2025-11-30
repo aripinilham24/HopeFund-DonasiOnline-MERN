55ae7eef76d989fcbc5a6036955c709efe2f7f57
aripinilham24 <muhamadarifinilham24@gmail.com>
2025-11-30 09:46:46 +0700

    update api

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
+let url = "localhost:5000/api"
+
+if(process.env.MODE === "PRODUCTION") {
+    url = "hopefund-be-production.up.railway.app/api"
+}
+
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: url,
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
