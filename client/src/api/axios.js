36f10dabb5247a9063dfe1922cb7436a0aa047a3
Aripin <147223158+aripinilham24@users.noreply.github.com>
2026-01-04 22:41:11 +0700

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,12 +3,13 @@ import axios from "axios";
 let url = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 const api = axios.create({
-    baseURL: `${url}/api/`,
+    baseURL: `${url}api/`,
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
     },
 });
+
 
 
 export {api, url}
