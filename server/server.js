2bbd5cf14ecbf23c9c7d7497905a69b07715c61f
aripinilham24 <muhamadarifinilham24@gmail.com>
2026-07-01 06:28:43 +0700

    api health

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ import { fileURLToPath } from "url";
 dotenv.config();
 
 let url = "http://localhost:5173";
-if(process.env.MODE === "PRODUCTION") {
+if (process.env.MODE === "PRODUCTION") {
   url = process.env.URL_FE;
 }
 
@@ -21,7 +21,7 @@ const __dirname = path.dirname(__filename);
 
 app.use(
   "/uploads/image/",
-  express.static(path.join(__dirname, "./uploads/image/"))
+  express.static(path.join(__dirname, "./uploads/image/")),
 );
 
 app.use(
@@ -30,11 +30,15 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
-  })
+  }),
 );
 app.use(express.json());
 
 await connectDB();
+
+app.use("api/health", (req, res) => {
+  res.status(200).json({ message: "Server is running!" });
+});
 
 app.use("/api/campaigns", campaignRoutes);
 app.use("/api/auth", authRoutes);
@@ -42,5 +46,5 @@ app.use("/api/payment", donateRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
-  console.log(`\n\nServer running on http://localhost:${PORT}`)
+  console.log(`\n\nServer running on http://localhost:${PORT}`),
 );
